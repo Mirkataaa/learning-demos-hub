62cfd2a2add917ffef67f62b78707936736c2628
Mirkataaa <156722755+Mirkataaa@users.noreply.github.com>
2025-10-21 16:30:18 +0300

    real-estate-tailwind:feat: add navigation & implement dark/light toggle

---
+++ tailwind-real-estate/tailwind-real-estate/src/script.js
@@ -0,0 +1,20 @@
+const toggleDarkMode = document.getElementById("toggleDarkMode");
+
+toggleDarkMode.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark");
+  if (document.documentElement.classList.contains("dark")) {
+    localStorage.theme = "dark";
+  } else {
+    localStorage.theme = "light";
+  }
+});
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
